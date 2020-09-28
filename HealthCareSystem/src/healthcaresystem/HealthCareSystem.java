d53e965189fb17847722be8837260b5b9854c65d
SGO-Nova <51762752+SGO-Nova@users.noreply.github.com>
2020-09-28 08:46:04 -0500

    Added timer procedure location for 8 and 9 pm actions

--- HealthCareSystem/src/healthcaresystem/HealthCareSystem.java
+++ HealthCareSystem/src/healthcaresystem/HealthCareSystem.java
@@ -1,16 +1,41 @@
 package healthcaresystem;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 public class HealthCareSystem extends Application {
     
+    private String pattern = "HH:mm:ss";
+    
     @Override
     public void start(Stage stage) throws Exception {
+        
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            LocalDateTime nowTime = LocalDateTime.now();
+            if(((String)LocalDateTime.now().format(DateTimeFormatter.ofPattern(pattern))).equals("20:00:00")){
+                System.out.println("CLEARING ALL NO-SHOW APPOINTMENTS");
+            }
+            if(((String)LocalDateTime.now().format(DateTimeFormatter.ofPattern(pattern))).equals("21:00:00")){
+                System.out.println("GENERATING DAILY REPORTS");
+            }
+        }),
+             new KeyFrame(Duration.seconds(1))
+                
+        );
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern(pattern)));
+        
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         
         Scene scene = new Scene(root);
@@ -18,6 +43,7 @@ public class HealthCareSystem extends Application {
         stage.setScene(scene);
         stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
+        
     }
 
     public static void main(String[] args) {
